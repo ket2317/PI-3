ad4dfc4672229b22086014e7b33e930414bdf394
ket2317 <ket200168@gmail.com>
2026-08-25 10:37:13 -0600

    creacion de endpoint de login

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI,Request
 from fastapi.templating import Jinja2Templates
+from app.routers.auth import router as auth_router
+
 
 templates = Jinja2Templates(directory="app/templates")
 
 app = FastAPI()
+app.include_router(auth_router)
 
 @app.get("/")
 def root(request: Request):
