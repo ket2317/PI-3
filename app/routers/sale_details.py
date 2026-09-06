c817d5112e32c863ee5a20082faab5e16bf758b4
keylaNavarro <knavarro11@ucol.mx>
2026-09-05 19:37:56 -0600

    Actualizar inventario con ventas
    Se agregó la actualización automática del inventario al registrar una venta y la validación de stock disponible.

--- app/routers/sale_details.py
+++ app/routers/sale_details.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import get_db
 from app.models.sale_detail import SaleDetail
 from app.schemas.sale_detail import SaleDetailCreate, SaleDetailResponse
+from app.models.inventory import Inventory
+from app.models.sale import Sale
+
 
 
 router = APIRouter(
@@ -22,9 +25,43 @@ def create_sale_detail(
     detail: SaleDetailCreate,
     db: Session = Depends(get_db)
 ):
+    # Buscar la venta para saber de qué sucursal es
+    sale = db.query(Sale).filter(
+        Sale.id == detail.venta_id
+    ).first()
+
+    if not sale:
+        raise HTTPException(
+            status_code=404,
+            detail="Venta no encontrada"
+        )
+
+    # Buscar el producto en el inventario de esa sucursal
+    inventory = db.query(Inventory).filter(
+        Inventory.sucursal_id == sale.sucursal_id,
+        Inventory.producto_id == detail.producto_id
+    ).first()
+
+    if not inventory:
+        raise HTTPException(
+            status_code=404,
+            detail="Producto no encontrado en el inventario"
+        )
+
+    # Comprobar que haya suficiente producto
+    if inventory.cantidad < detail.cantidad:
+        raise HTTPException(
+            status_code=400,
+            detail="Inventario insuficiente"
+        )
+
+
     new_detail = SaleDetail(**detail.model_dump())
 
     db.add(new_detail)
+
+    inventory.cantidad -= detail.cantidad
+    
     db.commit()
     db.refresh(new_detail)
 
